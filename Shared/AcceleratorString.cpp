01c241064f8253eb0cb86208400b23b193c0e0d1
dan.g <daniel.godson@gmail.com>
2018-05-11 21:08:52 +1000

    Fix assert on non-latin languages

--- Shared/AcceleratorString.cpp
+++ Shared/AcceleratorString.cpp
@@ -309,9 +309,12 @@ BOOL CAcceleratorString::EnsureUniqueAccelerators(CStringArray& aText)
 			}
 
 			// This means that every char in 'sText' appears in 'sAccelerators'
+			// or the string is in a non-latin language eg. Chinese
 			// Use the next char 'sAccelerators' appearing in 'sText'
 			int nAccel = Misc::FindFirstOf(sText, sAccelerators, FALSE);
-			ASSERT(nAccel != -1);
+
+			if (nAccel == -1) // non-latine language
+				break;
 
 			cAccel = sAccelerators[nAccel];
 			ASSERT(cAccel != NULLCHAR);
