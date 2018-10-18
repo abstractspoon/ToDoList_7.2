0996a30721338d39f0689e4b0f2810108a63ca04
dan.g <abstractspoon@users.noreply.github.com>
2018-10-18 13:25:48 +1100

    Removed unnecessary function override

--- Shared/EnCheckComboBox.h
+++ Shared/EnCheckComboBox.h
@@ -40,7 +40,6 @@ protected:
 	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(CEnCheckComboBox)
 protected:
-	virtual void PreSubclassWindow();
 	//}}AFX_VIRTUAL
 	
 	// Implementation
