8e738196061633c19f1b7462d9744dd5238419f8
dan.g <daniel.godson@gmail.com>
2018-10-20 19:56:21 +1100

    Adjust comment field when checkbox is hidden

--- ToDoList/resource.h
+++ ToDoList/resource.h
@@ -804,6 +804,7 @@
 #define IDC_DELETE                      1517
 #define IDC_SHOWFULLPATHINSTICKY        1518
 #define IDC_RELOADICONS                 1519
+#define IDC_COMMENTLABEL                1520
 #define IDC_TASKLIST                    2000
 #define IDD_IMPORT_OUTLOOK_DIALOG       2000
 #define IDI_OUTLOOK                     2001
@@ -2218,7 +2219,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        311
 #define _APS_NEXT_COMMAND_VALUE         34617
-#define _APS_NEXT_CONTROL_VALUE         1520
+#define _APS_NEXT_CONTROL_VALUE         1521
 #define _APS_NEXT_SYMED_VALUE           108
 #endif
 #endif
