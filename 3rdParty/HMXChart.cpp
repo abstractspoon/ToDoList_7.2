375b9b75e7df7ad5b8ff68cc8da4c6e9430e9b54
dan.g <abstractspoon@users.noreply.github.com>
2019-01-24 17:41:32 +1100

    Merge fixes from ToDoList_Dev

--- 3rdParty/HMXChart.cpp
+++ 3rdParty/HMXChart.cpp
@@ -97,6 +97,7 @@ void CHMXChart::DoPaint(CDC& dc, BOOL bPaintBkgnd)
 	if (!m_penGrid.GetSafeHandle())
 		m_penGrid.CreatePen(PS_SOLID, 1, m_clrGrid);
 
+	dc.SetTextColor(GetSysColor(COLOR_WINDOWTEXT));
 	DrawDatasets(dc);
 	DrawGrid(dc);
 	DrawTitle(dc);
