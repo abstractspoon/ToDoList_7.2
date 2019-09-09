9af0365bfe8c91804c15d67add646a7a603a92cd
dan.g <abstractspoon@users.noreply.github.com>
2019-09-09 11:21:05 +1000

    Merge regional settings change fix from ToDoList_7.2

--- ToDoList/ToDoListWnd.cpp
+++ ToDoList/ToDoListWnd.cpp
@@ -196,6 +196,7 @@ CToDoListWnd::CToDoListWnd()
 	m_bSettingAttribDefs(FALSE),
 	m_bReshowTimeTrackerOnEnable(FALSE),
 	m_bPromptLanguageChangeRestartOnActivate(FALSE),
+	m_bPromptRegionalSettingsRestartOnActivate(-1),
 	m_bIgnoreNextResize(FALSE),
 	m_bAllowForcedCheckOut(FALSE)
 {
@@ -11081,6 +11082,18 @@ void CToDoListWnd::OnActivateApp(BOOL bActive, HTASK hTask)
 			}
 		}
 
+		if (m_bPromptRegionalSettingsRestartOnActivate > 0)
+		{
+			// Once per session
+			m_bPromptRegionalSettingsRestartOnActivate = FALSE;
+
+			if (MessageBox(IDS_RESTARTTOUPDATESETTINGS, 0, MB_YESNO) == IDYES)
+			{
+				DoExit(TRUE);
+				return;
+			}
+		}
+		
 		if (GetTDCCount() && (!m_hwndLastFocus || Prefs().GetAutoFocusTasklist()))
 		{
 			PostMessage(WM_APPRESTOREFOCUS, 0L, (LPARAM)GetToDoCtrl().GetSafeHwnd());
@@ -12972,11 +12985,9 @@ void CToDoListWnd::OnSettingChange(UINT uFlags, LPCTSTR lpszSection)
 	// Prompt to restart app whenever Regional settings change
 	if ((uFlags == 0) && (StrCmp(lpszSection, _T("intl")) == 0))
 	{
-		if (MessageBox(IDS_RESTARTTOUPDATESETTINGS, 0, MB_YESNO) == IDYES)
-		{
-			DoExit(TRUE);
-			return;
-		}
+		// Once per session only
+		if (m_bPromptRegionalSettingsRestartOnActivate == -1)
+			m_bPromptRegionalSettingsRestartOnActivate = TRUE;
 	}
 	else if (StrCmp(lpszSection, _T("TraySettings")) == 0)
 	{
