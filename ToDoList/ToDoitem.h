f93356ea9e9aed4cb88196cfa01b71aa997465a4
dan.g <abstractspoon@users.noreply.github.com>
2019-10-17 11:51:58 +1100

    Add debug helper

--- ToDoList/ToDoitem.h
+++ ToDoList/ToDoitem.h
@@ -216,6 +216,10 @@ public:
 	void DeleteAll() { CleanUp(); }
 	
 	int MoveSubTask(int nPos, TODOSTRUCTURE* pTDSDestParent, int nDestPos);
+
+#ifdef _DEBUG
+	void Trace(const CString& sIndent) const;
+#endif
 	
 protected:
 	DWORD m_dwID;
